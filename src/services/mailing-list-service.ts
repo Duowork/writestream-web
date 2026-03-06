83424c6d6b2942b8bcca065af0ac1b70b7ff2396
Romeo <romeomeshack34@gmail.com>
2026-03-06 03:46:49 +0100

    Modified

--- src/services/mailing-list-service.ts
+++ src/services/mailing-list-service.ts
@@ -24,7 +24,7 @@ export async function subscribeToMailingList(name: string, email: string) {
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({ email }),
+            body: JSON.stringify({name, email }),
         });
 
         const result = await response.json();
